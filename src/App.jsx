9349d113eb929a670cf6a6d7aa3ff2d19e22b3e4
Andrew Kononenko <wastardy.k@gmail.com>
2025-06-21 22:37:31 +0300

    feat(app): implement routing with Navbar and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { Footer } from "./components/Footer";
 
 function App() {
-  return <h1 className="app">Hi there</h1>;
+  return (
+    <h1 className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </h1>
+  );
 }
 
 export default App;
