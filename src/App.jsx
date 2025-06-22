f6ba6401b015c159e9c27d9bd8ebe504d5c44c5c
Andrew Kononenko <wastardy.k@gmail.com>
2025-06-22 07:39:31 +0300

    feat(app): add routing for Menu, About, and Contact pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,25 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
 import { Footer } from "./components/Footer";
+import { Contact } from "./pages/Contact";
+import { About } from "./pages/About";
 
 function App() {
   return (
-    <h1 className="App">
+    <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </BrowserRouter>
-    </h1>
+    </div>
   );
 }
 
